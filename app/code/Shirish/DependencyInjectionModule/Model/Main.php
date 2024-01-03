4f49e1e06eb51a3d8c7c093cd0c68c22e38d96ec
Shirish Jaiswal <shirish.jaiswal@hbwsl.com>
2024-01-03 18:41:08 +0530

    adding the Main as dependency dynamically using di.xml

--- app/code/Shirish/DependencyInjectionModule/Model/Main.php
+++ app/code/Shirish/DependencyInjectionModule/Model/Main.php
@@ -3,7 +3,13 @@
 namespace Shirish\DependencyInjectionModule\Model;
 
 class Main {
+    protected array $data;
+    public function __construct(array $data = [])
+    {
+        $this->data = $data;
+    }
     public function getId() : string {
-        return "Class Main";
+        return $this->data["id"];
+        // data paramater should match which is present inside the di.xml
     }
 }
