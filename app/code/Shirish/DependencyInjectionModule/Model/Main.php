8cf3048b17180467f326af36669524dbd1fb80c9
Shirish Jaiswal <shirish.jaiswal@hbwsl.com>
2024-01-04 10:38:13 +0530

    Using the di.xml for dynamically adding the data inside the Main class Id

--- app/code/Shirish/DependencyInjectionModule/Model/Main.php
+++ app/code/Shirish/DependencyInjectionModule/Model/Main.php
@@ -5,7 +5,22 @@ namespace Shirish\DependencyInjectionModule\Model;
 
 class Main {
 
-    public function getId () :string{
-        return "Class Main Id";
+
+    // if we want the Id data to be dynamically come then we make use of array $data. 
+    // The id will come from it.
+    // but the data in $data will come from the di.xml file 
+
+    // To store the data coming in $data we will store it in data
+    protected $data;
+
+    public function __construct(array $data=[])
+    {
+        $this->data = $data;
+    }
+    
+    public function getId () :string
+    {
+        // This id will come from di.xml
+        return $this->data["id"];
     }
 }
