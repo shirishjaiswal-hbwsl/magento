8c80d8fb12a002d108954b2e957d96a4c85ba52e
Shirish Jaiswal <shirish.jaiswal@hbwsl.com>
2024-01-04 10:10:35 +0530

    Fresh Start Dependency Injection

--- app/code/Shirish/DependencyInjectionModule/Block/Example.php
+++ app/code/Shirish/DependencyInjectionModule/Block/Example.php
@@ -3,22 +3,10 @@ namespace Shirish\DependencyInjectionModule\Block;
 
 use Magento\Framework\View\Element\Template\Context;
 use Magento\Framework\View\Element\Template;
-use Shirish\DependencyInjectionModule\Model\Main;
 
 class Example extends Template {
-    // Magento will automatically inject the dependencies which are present inside the cosntructor
-    protected $main;
-    public function __construct(
-        Context $context,
-        Main $main,
-        array $data = []
-        )
+    public function __construct(Context $context, array $data = [])
     {
         parent::__construct($context, $data);
-        $this->main = $main;
-    }
-
-    public function getMain() :Main {
-        return $this->main;
     }
 }
